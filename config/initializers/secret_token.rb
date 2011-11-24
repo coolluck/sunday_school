53416e7bdb4868d4098892fb8fd71dd2f532bc91
崔 珉秀 <coolluck@coolluck-macbook-air.local>
2011-11-24 19:31:01 +0900

    initialize git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SundaySchool::Application.config.secret_token = 'f86e785ea7603f6253534f9e161a38efa2549e28bf5bc8a6c49de0fbc156489b80ea14eb4c47709a392090b6bb5eb96e1c403a83a3f7f2adb31ab6d00ae26958'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SundaySchool::Application.config.session_store :cookie_store, key: '_sunday_school_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SundaySchool::Application.config.session_store :active_record_store
